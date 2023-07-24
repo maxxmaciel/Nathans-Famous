827712815c33708136b36c4a3d31162149210061
maxxmaciel <88160151+maxxmaciel@users.noreply.github.com>
2023-07-24 18:07:49 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-function render_main() {
+function render_main(text = "") {
   load_nodes(g.nodes);
-  let text = "";
 
   for (let idx in data) {
     let el = data[idx];
@@ -18,16 +17,15 @@ function render_main() {
           <img src="${std.ensure(el, KEY.IMAGE, "")}" />
         </div>
       </div> 
-    <div class="title_toppings c_category">
+     <div class="title_toppings c_category">
       <p> ${std.ensure(el, KEY.EXTRA_TOPPINGS_NAME, '')} </p>
-        <div class="c_extra_toppings">
-           ${ensureExtraToppings(el.extra_toppings)}
-        </div>
-        </div> 
-      </div> 
-    </div>
+      <div class="c_extra_toppings">
+         ${ensureExtraToppings(el.extra_toppings)}
+      </div>
+    </div> 
+  </div> 
+</div>
     `;
-
   }
   g.nodes.main.nodes.html(text);
 }
@@ -42,11 +40,8 @@ function reassign_node(vt_node) {
 
   if (vt_node.nodes.length == 0) {
     console.log(`o node ${vt_node.selector}não existe`);
-    return;
-  }
-  if (std.is_null(vt_node.events)) {
-    return;
-  } else {
+
+  } else if (!std.is_null(vt_node.events)) {
     for (var ev_nm in vt_node.events) {
       var ev = vt_node.events[ev_nm];
       vt_node.nodes.each((i, el) => $(el).on(ev_nm, ev));
@@ -54,51 +49,40 @@ function reassign_node(vt_node) {
   }
 }
 
-function replaceAndFindParentheses(text) {
+function replaceParentheses(text) {
   var r2 = null;
   var r1 = text.replace(REGEX.FIND_PARENTHESES, (a) => {
-    r2 = a; ''
+    r2 = a;
     return "";
   });
   return { name: r1, obs: r2 };
 }
 
-
-function replaceDotsWithCommas(texto) {
-  const textoComVirgulas = texto.replace(/\./g, ',');
-  return textoComVirgulas;
-}
-
-function ensureProduct(p) {
-  let text = ""
+function ensureProduct(p, text = "") {
   for (let idx in p) {
     let el = p[idx];
-    var r = replaceAndFindParentheses(std.ensure(el, KEY.NAME, ""));
+    var r = replaceParentheses(std.ensure(el, KEY.NAME, ""));
 
     text += /*html*/ `
     <div class="product"> 
       <div class="flex-direction-column w-70">
         <div class="c-1"> 
-          <p> ${r.name} ${!std.is_null(r.obs) ? /*html*/ ` <span>${r.obs} </span>` : ""}</p>
-     
+          <p> ${r.name} ${!std.is_null(r.obs) ? /*html*/ ` <span>${r.obs} </span>` : ""}</p>  
         <div class="money">R$ ${replaceDotsWithCommas(std.ensure(el, KEY.VALUE, 0).toFixed(2))} </div>  
       </div>
        <div class="c-2">
          ${std.ensure(el, KEY.SIZE, "")}
        </div>
       </div>
-    </div>
-  
-    `;
+    </div> `;
   }
   return text;
 }
 
-function ensureExtraToppings(t_2) {
-  let text = ""
+function ensureExtraToppings(t_2, text = "") {
   for (let idx in t_2) {
     let el = t_2[idx];
-    var r = replaceAndFindParentheses(std.ensure(el, KEY.NAME, ""));
+    var r = replaceParentheses(std.ensure(el, KEY.NAME, ""));
 
     text += /*html*/ `
       <div class="extra_toppings">+${r.name} ${!std.is_null(r.obs) ? /*html*/ `<span>${r.obs}</span>` : ""
@@ -112,21 +96,5 @@ function ensureExtraToppings(t_2) {
   }
   return text;
 }
-/* 
-function showNode(x) {
 
-  let offset = x > g.card_on ? -50 : +50;
-  let y = g.cards[x];
-  let a = g.cards[g.card_on];
-  let on = g.nodes[a].nodes, off = g.nodes[y].nodes;
-
-  on.hide();
-  off.show();
-
-  off.css(CSS_PROPERTY_NAME.LEFT, offset + SIZE.VW);
-  off.animate({ left: 0 }, {
-    duration: 800
-  });
-
-  g.card_on = x;
-}; */
+const replaceDotsWithCommas = (texto) => texto.replace(/\./g, ',');
